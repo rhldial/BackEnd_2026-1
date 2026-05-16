33db371f66fc3c85e0c00903da83510266eb0438
박준용 <skjuny112@gmail.com>
2026-05-16 09:08:16 +0900

    introduce?name=이름 구현

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,5 +17,11 @@ public class HelloController {
     @GetMapping("/hello2")
     public String hello2() {
         return "hello";
+    }
+
+    @ResponseBody
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
     }
 }
